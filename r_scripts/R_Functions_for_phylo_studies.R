05fab55275b3bdee4913c5379d7e67cdd88e7c02
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-09-25 18:05:41 +1000

    Function: clean up phy & datasets
    compare phylo vs dataset and clean up missing data for both

--- r_scripts/R_Functions_for_phylo_studies.R
+++ r_scripts/R_Functions_for_phylo_studies.R
@@ -15,3 +15,26 @@ rescale_phy = function(phy_nwk, scale = 1, ladderize = TRUE) {
     return (phy_nwk)
   } 
 }
+
+# Function to compare phy_nwklo vs dataset and clean up missing data
+clean_up_data = function(phy_nwk, dat_csv) {
+  
+  phy_nwkOnly <- setdiff(phy_nwk$tip.label, rownames(dat_csv))
+  datOnly <- setdiff(rownames(dat_csv), phy_nwk$tip.label)
+  
+  if (length(phy_nwkOnly) > 0) {
+    phy_nwk <- drop.tip(phy_nwk, phy_nwkOnly)
+    message("The following taxa were deleted from the tree: "); print(phy_nwkOnly)
+  }
+  
+  if (length(datOnly) > 0) {	
+    dat_csv <- dat_csv[-match(datOnly, rownames(dat_csv)),]
+    message("The following taxa were deleted from the dataset: "); print(datOnly) 
+  }
+  
+  dat_csv <- dat_csv[phy_nwk$tip.label,]
+  if (rownames(dat_csv) == phy_nwk$tip.label) {
+    return (list(phy_nwk, dat_csv))
+  }
+  #TODO else to catch error if rownames(dat_csv) != phy_nwk$tip.label
+}
