91c249defb7060c13f34c335fdd5f5ba5aca8f18
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-09-25 18:08:34 +1000

    Function: plot discrete data on trees
    plot discrete data on trees. It can not be used with rescale trees yet

--- r_scripts/R_Functions_for_phylo_studies.R
+++ r_scripts/R_Functions_for_phylo_studies.R
@@ -38,3 +38,24 @@ clean_up_data = function(phy_nwk, dat_csv) {
   }
   #TODO else to catch error if rownames(dat_csv) != phy_nwk$tip.label
 }
+
+# Function to plot biogeo data on trees
+plot_biogeo = function (phy_nwk, dat_csv) {
+ 
+    # TODO: add the path to folders 
+    pdf('plot.pdf', width=8, height=10, paper = 'a4')
+    plot(phy_nwk, cex=0.5, label.offset = (ncol(dat_csv) + 1), no.margin = TRUE)
+    eixo_x = max(nodeHeights(phy_nwk))
+    eixo_y = length(phy_nwk$tip.label)
+    
+    for (i in 1:ncol(dat_csv)) {
+      dat_col = dat_csv[i]
+      label = dat_col[match(phy_nwk$tip.label, row.names(dat_col)), ]
+      
+      points(rep(eixo_x + i, eixo_y), 1:(eixo_y), pch=22, bg = label, cex=1)
+      text((eixo_x + i), (eixo_y + 0.8), colnames(dat_col), cex=0.4, srt=90, adj=0.0)
+    }
+    
+    dev.off()
+    return (list(phy_nwk, dat_csv))
+}
