2b32bcbd5d80fabb947b4938e6fabe3e02a96469
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-09-29 23:35:35 +1000

    reorganization

--- r_scripts/R_Functions_for_phylo_studies.R
+++ r_scripts/R_Functions_for_phylo_studies.R
@@ -3,100 +3,82 @@ library(phytools)
 library(geiger)
 
 # Function to rescale & ladderize phylogenies 
-rescale_phy = function(phy_nwk, scale = 1, ladderize = TRUE) {
-  #From: http://blog.phy_nwktools.org/2012/02/quicker-way-to-rescale-total-length-of.html
-  phy_nwk$edge.length = phy_nwk$edge.length/max(nodeHeights(phy_nwk)[,2])*scale
+rescale_phy = function(phy, scale = 1, ladderize = TRUE) {
+  #From: http://blog.phytools.org/2012/02/quicker-way-to-rescale-total-length-of.html
+  phy$edge.length = phy$edge.length/max(nodeHeights(phy)[,2])*scale
   
   if (ladderize == TRUE) {
-    phy_nwk_ladderize = ladderize(phy_nwk)
-    return (phy_nwk_ladderize)
+    phy_ladderize = ladderize(phy)
+    return (phy_ladderize)
   }
   else {
-    return (phy_nwk)
+    return (phy)
   } 
 }
 
-# Function to compare phy_nwklo vs dataset and clean up missing data
-clean_up_data = function(phy_nwk, dat_csv) {
+# Function to compare phylo vs dataset and clean up missing data
+pw_Cleanup_data = function(phy, dat.csv) {
   
-  phy_nwkOnly <- setdiff(phy_nwk$tip.label, rownames(dat_csv))
-  datOnly <- setdiff(rownames(dat_csv), phy_nwk$tip.label)
+  dat.csv <- dat.csv[phy$tip.label,]
   
-  if (length(phy_nwkOnly) > 0) {
-    phy_nwk <- drop.tip(phy_nwk, phy_nwkOnly)
-    message("The following taxa were deleted from the tree: "); print(phy_nwkOnly)
-  }
-  
-  if (length(datOnly) > 0) {	
-    dat_csv <- dat_csv[-match(datOnly, rownames(dat_csv)),]
-    message("The following taxa were deleted from the dataset: "); print(datOnly) 
-  }
-  
-  dat_csv <- dat_csv[phy_nwk$tip.label,]
-  if (rownames(dat_csv) == phy_nwk$tip.label) {
-    return (list(phy_nwk, dat_csv))
-  }
-  #TODO else to catch error if rownames(dat_csv) != phy_nwk$tip.label
-}
-
-# Function to plot biogeo data on trees
-plot_biogeo = function (phy_nwk, dat_csv) {
- 
-    # TODO: add the path to folders 
-    pdf('plot.pdf', width=8, height=10, paper = 'a4')
-    plot(phy_nwk, cex=0.5, label.offset = (ncol(dat_csv) + 1), no.margin = TRUE)
-    eixo_x = max(nodeHeights(phy_nwk))
-    eixo_y = length(phy_nwk$tip.label)
-    
-    for (i in 1:ncol(dat_csv)) {
-      dat_col = dat_csv[i]
-      label = dat_col[match(phy_nwk$tip.label, row.names(dat_col)), ]
+  if (rownames(dat.csv) == phy$tip.label) {
+    } else {
       
-      points(rep(eixo_x + i, eixo_y), 1:(eixo_y), pch=22, bg = label, cex=1)
-      text((eixo_x + i), (eixo_y + 0.8), colnames(dat_col), cex=0.4, srt=90, adj=0.0)
-    }
-    
-    dev.off()
-    return (list(phy_nwk, dat_csv))
-}
+      #add info about read.csv, rownames = TRUE
+      phyOnly <- setdiff(phy$tip.label, rownames(dat.csv))
+      datOnly <- setdiff(rownames(dat.csv), phy$tip.label)
+      
+      if (length(phyOnly) > 0) {
+        phy <- drop.tip(phy, phyOnly)
+        message("The following taxa were deleted from the tree: "); print(phyOnly)
+        }
+      
+      if (length(datOnly) > 0) {
+        dat.csv <- dat.csv[-match(datOnly, rownames(dat.csv)),]
+        message("The following taxa were deleted from the dataset: "); print(datOnly)
+        }
+      }
+  message('You are ready to proceed! see phy: [[1]], csv: [[2]]')
+  return (list(phy, dat.csv))
+  }
 
 #Function to get best model of discrete character evolution
-eval_fitDiscrete_models = function (phy_nwk, dat_csv, numcol = 1){
+pw_fitDiscreteModels = function (phy, dat.csv, numcol = 1){
   
-  dat_vector = as.factor(dat_csv[, numcol]) 
-  names(dat_vector) = row.names(dat_csv)
+  dat.vector = as.factor(dat.csv[, numcol]) 
+  names(dat.vector) = row.names(dat.csv)
   
-  model_choices = c('ER', 'SYM', 'ARD')
+  model.choices = c('ER', 'SYM', 'ARD')
     
-  fit_model = list()
+  fit.model = list()
   for (i in 1:3) {
-    fit = fitDiscrete(phy_nwk, dat_vector, model = model_choices[i])
-    fit_model[[model_choices[i]]] = fit
+    fit = fitDiscrete(phy, dat.vector, model = model.choices[i])
+    fit.model[[model.choices[i]]] = fit
   }
   
   #csv table
-  lnL      = c(fit_model$ER$opt$lnL, fit_model$SYM$opt$lnL, fit_model$ARD$opt$lnL)
-  aicc     = c(fit_model$ER$opt$aicc, fit_model$SYM$opt$aicc, fit_model$ARD$opt$aicc)
-  del_aicc = c(aicw(aicc)$delta)
-  wts_aicc = c(aicw(aicc)$w)
+  lnL      = c(fit.model$ER$opt$lnL, fit.model$SYM$opt$lnL, fit.model$ARD$opt$lnL)
+  aicc     = c(fit.model$ER$opt$aicc, fit.model$SYM$opt$aicc, fit.model$ARD$opt$aicc)
+  del.aicc = c(aicw(aicc)$delta)
+  wts.aicc = c(aicw(aicc)$w)
   
-  output_csv = matrix( , 3, 4, dimnames=list(c("ER", "SYM", "ARD"),
+  output.csv = matrix( , 3, 4, dimnames=list(c("ER", "SYM", "ARD"),
                                              c("lnL", "AICc", "delta AICc", "AICc weights")))
-  output_csv[,1] = lnL  
-  output_csv[,2] = aicc
-  output_csv[,3] = del_aicc
-  output_csv[,4] = wts_aicc
+  output.csv[,1] = lnL  
+  output.csv[,2] = aicc
+  output.csv[,3] = del.aicc
+  output.csv[,4] = wts.aicc
   
-  write.csv(output_csv, file = 'model_selection.csv')
+  write.csv(output.csv, file = 'model_selection.csv')
   
-  best_fitted_model = fit_model[which.min(aicc)]
+  best.fitted.model = fit.model[which.min(aicc)]
   
-  return (list(best_fitted_model, dat_vector))
+  return (best.fitted.model)
 }
 
 # Fuction to make simmaps and plot 1st simmap result
-plot_simmmap_pdf = function (phy_nwk, dat_vector, model, nsim = 100){
-  mtrees = make.simmap(phy_nwk, dat_vector, model, nsim)
+pw_plot_Simmmap = function (phy, dat_vector, model, nsim = 100){
+  mtrees = make.simmap(phy, dat_vector, model, nsim)
   stoch_map = describe.simmap(mtrees, plot=FALSE)
   
   cols = setNames(palette()[1:length(unique(dat_vector))], sort(unique(dat_vector)))
---
+++ r_scripts/R_plot_functions.R
@@ -0,0 +1,125 @@
+library(phytools)
+
+pw_plot_Phylomorphospace = function(phy, dat.csv, anc.estimates = FALSE, 
+                                             pdf.name = NULL, ...) {
+  # Plot a phylomorphoscape with information of ancestral area estimates
+  #
+  # Args:
+  #  phy:  Phylogeny stored as the class "phylo"
+  #  dat.csv:  CSV file with species names as rownames.
+  #            Be sure your original CSV has four columns being the 1º comprised of species 
+  #              name, 2º of a discrete character and 3º e 4º of lat and lon values (in decimals)
+  #            Be sure phy and dat.csv have same length and are in the same order.
+  #              Use PwCleanUp to prepare the input data
+  #  anc.estimates: estimates of ancestral states obtained from Ace (package ape)
+  #                 if anc.estimates is set to TRUE, it plots estimates of ancestral states 
+  #                 at each node of the phylo
+  #  pdf.name: name of pdf plot saved in the working directory 
+  # Returns:
+  #  NULL. PDF plot is saved in the working directory
+  
+  # Error handling
+  if (rownames(dat.csv) != phy$tip.label){
+    stop('rownames of dat.csv and tip.labels of phy do not match each other. Use PwCleanUp!')
+  }
+  
+  if (ncol(dat.csv) != 3) {
+    stop('dat.csv must have 3 data columns')
+  }
+  
+  if (is.factor(dat.csv[,1]) == FALSE) {
+    stop('The columns must contains a discrete (str: factor) and two continuous characters, respectively')
+  }
+  
+  if (is.null(pdf.name)) {
+    stop('provide a pdf.name to your plot - pdf.name = NULL')
+  }
+#  if (anc.estimates  ) {
+#    stop('provide a ** ')
+  
+  # Vectors
+  vec.discrete   = as.factor(dat.csv[,1]);  names(vec.discrete)   = row.names(dat.csv) 
+  vec.continuous = dat.csv[,2:3];           # names(vec.continuous) = row.names(dat.csv) 
+  
+  # Plot parameters
+  cols = setNames(rainbow(nlevels(vec.discrete), alpha = 1.0)
+                  [1:length(unique(vec.discrete))], sort(unique(vec.discrete)))
+  
+  if (levels(vec.discrete) == names(cols)){
+    levels(vec.discrete) = cols
+  } else {
+    stop('levels of the discrete characters must match to the names of assigned colors')
+  }
+  
+  col.tips = as.character(vec.discrete);   names(col.tips) <- phy$tip.label
+  
+  # Plot - save 'pdf.name' in the working directory
+  name = paste(pdf.name, '.pdf', sep = '')
+  pdf(name, width=7, height=7)
+  
+  phylomorphospace(phy, vec.continuous, ftype="off", node.size=rep(0,2))
+  tiplabels(pie=to.matrix(col.tips, levels(vec.discrete)), piecol=cols, cex= 0.5)
+  
+  legend("topleft", legend=names(cols), col=cols, fill = cols, bty = 'n')
+         
+  if (anc.estimates == TRUE) {
+    lik.anc = anc.estimates[[5]] # TODO - Fix this
+    nodelabels(pie=lik.anc, piecol=cols, cex=0.6)
+  }
+  dev.off()
+  
+  cmdstr = paste("open ", name, sep="")
+  system(cmdstr)
+  }
+
+
+pw_plot_BiogeoAreas = function(phy, dat.csv, pdf.name = NULL, ...) {
+  # Plot species area distributions on a phylo
+  #
+  # Args:
+  #  phy:  Phylogeny stored as the class "phylo"
+  #  dat.csv:  CSV file with species names as rownames and areas as colnames.
+  #            Be sure phy and dat.csv have same length and are in the same order.
+  #            Use PwCleanUp to prepare the input data
+  #  pdf.name: name of pdf plot saved in the working directory 
+  # Returns:
+  #  NULL. PDF plot is saved in the working directory
+  
+  # Error handling
+  if (rownames(dat.csv) != phy$tip.label){
+    stop('rownames of dat.csv and tip.labels of phy do not match. Use PwCleanUp!')
+  }
+  
+  if (is.null(pdf.name)){
+    stop('provide a pdf.name to your plot - pdf.name = NULL')
+  }
+  
+  # Plot parameters
+  label.offset = (max(nodeHeights(phy)) / 50) * ncol(dat.csv) 
+  eixo.x = max(nodeHeights(phy))
+  eixo.y = length(phy$tip.label)
+  pont.txt = label.offset / ncol(dat.csv)  
+  
+  # Plot - save 'pdf.name' in the working directory
+  name = paste(pdf.name, '.pdf', sep = '')
+  pdf(name, width=8, height=10, paper = 'a4')
+  
+  plot(phy, cex=0.5, label.offset = label.offset, no.margin = TRUE)
+  add.scale.bar()
+  
+  j = 0 # j is the counter for plotting points and texts in the x.axis
+  for (i in 1:ncol(dat.csv)) {
+    dat.col = dat.csv[i]
+    label = dat.col[match(phy$tip.label, row.names(dat.col)), ]
+    
+    points(rep(eixo.x + j, eixo.y), 1:(eixo.y), pch=22, bg = label, cex=1.7)
+    text((eixo.x + j), (eixo.y + 0.8), colnames(dat.col), cex=0.4, srt=90, adj=0.0)
+    j = j + pont.txt
+    
+  }
+  dev.off()
+  
+  # open plot in a PDF reader
+  cmdstr = paste("open ", name, sep="")
+  system(cmdstr)
+}
