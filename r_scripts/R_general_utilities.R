d1d4b1c944437e84fb2715734c44a71552867bb3
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-10-23 10:37:32 +1100

    new script to include general utilities in R

---
+++ r_scripts/R_general_utilities.R
@@ -0,0 +1,25 @@
+is.installed = function(pkg){ 
+  # Function to check whether package is installed or needs to be installed
+  #   from: http://stackoverflow.com/questions/9341635/check-for-installed-packages-before-running-install-packages  
+  # 
+  # Arg: 
+  #   pkg: package name
+  # Return: NULL. Ps: named package is installed case it isn't in the system
+  is.element(pkg, installed.packages()[,1])
+} 
+
+source_https <- function(url, ...) {
+  # Function to source R scripts hosted on github repository
+  #   from: https://tonybreyal.wordpress.com/2011/11/24/source_https-sourcing-an-r-script-from-github/
+  
+  # Arg: 
+  #   url: https github path (raw path)
+  # Return: all functions in the script
+  
+  require(RCurl)
+  
+  # parse and evaluate each .R script
+  sapply(c(url, ...), function(u) {
+    eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
+  })
+}
