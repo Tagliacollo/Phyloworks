56c973525a67896219bbf03d4720c4a5b9d16403
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-10-24 18:07:39 +1100

    small changes to add genes in the search

--- py_scripts/genBank.py
+++ py_scripts/genBank.py
@@ -1,71 +1,69 @@
 from Bio import Entrez
 import pandas as pd
+import time
 
-def gb_efetch(ssp_query, email):
-
+def gb_efetch(taxa, email, batch = 200, gene = 'None'):
+	
 	Entrez.email = email
 
-	# Parte 1: egquery
-	handle_egquery = Entrez.egquery(term = ssp_query)
-	egquery_read   = Entrez.read(handle_egquery)
+	# Parte 1: esearch 
+	query = '%s[Orgn]' % (taxa)
 
-	for row in egquery_read["eGQueryResult"]:
-		if row["DbName"]=="nuccore":
-			num_retmax = row["Count"]
+	if gene != 'None':
+		query = '%s[Orgn] AND %s[Gene]' % (taxa, gene)
 
-	handle_egquery.close()
-
-	# Parte 2: esearch 
-	handle_esearch = Entrez.esearch(db = "nucleotide", term = ssp_query, 
-									retmax = num_retmax, usehistory = "y")
+	handle_esearch = Entrez.esearch(db = 'nucleotide', term = query, retmax = 100000000)
 	esearch_read = Entrez.read(handle_esearch)
-	id_list = esearch_read["IdList"]
+	
+	id_list = esearch_read['IdList']
+	count   = esearch_read['Count']
 
-	handle_esearch.close()
-
-	# Parte 3: epost
-	handle_epost = Entrez.epost(db = "nucleotide", id=",".join(map(str,id_list)))
+	# Parte 2: epost
+	handle_epost = Entrez.epost(db = 'nucleotide', id=",".join(map(str,id_list)))
 	epost_read = Entrez.read(handle_epost)
-
 	handle_epost.close()
 
-	try:
-		handle_efetch = Entrez.efetch(db = "nucleotide", retmode="xml", webenv = epost_read["WebEnv"], 
-								 	  query_key = epost_read["QueryKey"])
+	# Parte 3: efetch
+	for start in range(0, int(count), batch):
+		end = min(int(count), start + batch)
+		print("Going to download record %i to %i" % (start + 1, end))
 
-	except HTTPError as err:
-		attempt = 1
-		if 500 <= err.code <= 599:
-			print("Received error from server %s" % err)
-		else:
-			raise
+		try:
+			handle_efetch = Entrez.efetch(db = "nucleotide", retmode = 'xml', rettype = 'gb', 
+										  webenv = epost_read["WebEnv"], query_key = epost_read["QueryKey"])
+
+		except HTTPError as err:
+			if 500 <= err.code <= 599:
+				print("Received error from server %s" % err)
+			else:
+				raise
 
 	return(handle_efetch)
 
-def gb_efetch_to_df(ssp_query, handle_efetch):
+def gb_efetch_to_df(taxa, handle_efetch):
 
 	efetch_read = Entrez.read(handle_efetch)
 	
 	df = pd.DataFrame()
 	for single in efetch_read:
-		df = pd.concat([df, gb_efetch_info(ssp_query, single)])
+		df = pd.concat([df, gb_efetch_parse(taxa, single)])
 
 	return(df)
 
-def gb_efetch_info(ssp_query, single_efetch):
+def gb_efetch_parse(ssp_query, single_efetch):
 
 	try:
 		gene_name   = single_efetch["GBSeq_feature-table"][1]['GBFeature_quals'][0]['GBQualifier_value']
 	except:
-		gene_name = 'unavailable'
+		gene_name = 'NaN'
 		pass
 
 	try:
 		seq = single_efetch['GBSeq_sequence']
 	except:
 		seq = 'NaN'
+		pass
 
-	out_name	= ssp_query	
 	org_name    = single_efetch["GBSeq_organism"]
 	gb_number   = single_efetch["GBSeq_primary-accession"]
 	seq_size    = single_efetch['GBSeq_feature-table'][0]['GBFeature_intervals'][0]['GBInterval_to']
@@ -77,11 +75,17 @@ def gb_efetch_info(ssp_query, single_efetch):
 	dat_list  = [ gene_name,   org_name,   gb_number,   seq_size,  nucl_seq,   upload_date,   paper_title]	
 	col_names = ['gene_name', 'org_name', 'gb_number', 'seq_size', 'nucl_seq', 'upload_date', 'paper_title']
 
-	df = pd.DataFrame(data=[dat_list], columns = col_names, index = [out_name])
+	df = pd.DataFrame(data=[dat_list], columns = col_names, index = [gene_name])
 
 	return(df)
 
-x = gb_efetch('apteronotus', 'victor_tagliacollo@yahoo.com.br')
-y = gb_efetch_to_df('apteronotus', x)
+
+
+
+ssp_query = 'Gymnotus'
+email     = 'victor_tagliacollo@yahoo.com.br'
+
+y = gb_efetch(ssp_query, email, gene = 'COI')
+z = gb_efetch_to_df(ssp_query, y)
 
 
