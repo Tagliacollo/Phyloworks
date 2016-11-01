3e4f4ca500310dbe5717b259dd9d848e43b0b914
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2016-11-01 21:18:45 +1100

    minor changes to run analysis

--- py_scripts/analyses_gb.py
+++ py_scripts/analyses_gb.py
@@ -1,8 +1,28 @@
 from genBank import *
+from utilities import output_path
+import os
 
-species = 'Apteronotidae'
+################
+
 email = 'victor_tagliacollo@yahoo.com.br'
 gene  = 'COI'
-program = 'muscle'
+cwd = '/Volumes/VATagliacollo/GitHub/Phyloworks/py_scripts'
 
-x = provides_species_get_align(query, gene, email,  batch=500, program = 'muscle')
+
+ssp_list = ['Gymnotus carapo']
+
+################ FASTA
+
+for sp in ssp_list:
+	os.chdir(output_path(cwd, sp))
+	print ('acquiring %s sequences for: %s' % (gene, sp))
+
+	provides_species_get_unalign_fasta(sp, gene, email, outfilename=sp, batch=500, rank = -4)
+
+
+################ ALIGNMENT
+
+
+
+################ PHYLOGENY
+
