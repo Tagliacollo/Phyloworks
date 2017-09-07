91cb3b585fbd0917ea9608af55df2e02d2113ddf
Tagliacollo <victor_tagliacollo@yahoo.com.br>
2017-09-07 14:52:58 -0300

    updated script - working

--- py_scripts/Gymn_py_script.py
+++ py_scripts/Gymn_py_script.py
@@ -3,7 +3,23 @@ from Bio.Nexus import Nexus
 from Bio import AlignIO
 from Bio import SeqIO
 from Bio.SeqUtils.CheckSum import seguid
+from io import StringIO
 
+
+def processed_data(aln_nex, cut, outfilename):
+
+    alns = charset_aln(aln_nex)
+    
+    ls_alns = []
+    for aln in alns:
+        gaps = cut_gap_in_blocks(aln[1], cut)
+        if gaps.get_alignment_length() == 0:
+            continue
+        else:
+            dupl = remove_dup_seqs(gaps)
+            ls_alns.append((aln[0], dupl))
+
+    export_nexus(ls_alns, outfilename)
 
 def charset_aln(aln_nex):
 # aln: 	nexus aln with charasets
@@ -12,17 +28,15 @@ def charset_aln(aln_nex):
 	dat = Nexus.Nexus()
 	dat.read(aln_nex)
 	aln_nex = AlignIO.read(open(aln_nex), "nexus")
-
-	aln_by_charset = []
+    
+	aln_by_charset   = []
 	for name in dat.charsets:
 		sites = dat.charsets[name]
 		start = min(sites)
 		stop  = max(sites) + 1
-		aln_by_charset.append(aln_nex[:, start:stop])
+		aln_by_charset.append((name, aln_nex[:, start:stop]))
 
 	return(aln_by_charset)
-
-
 
 def cut_gap_in_blocks(aln, cut):
 # From - https://stackoverflow.com/questions/28220301/python-remove-special-column-from-multiple-sequence-alignment
@@ -41,7 +55,7 @@ def cut_gap_in_blocks(aln, cut):
             else:
                 aln = aln[:, :i] + aln[:, i+ct:]
             n -= ct    #  seq. ct positions shorter
-        else:  # nothing to delete, proceed
+        else:          # nothing to delete, proceed
             i += 1
 
     return(aln)
@@ -55,7 +69,7 @@ def remove_dup_seqs(aln):
     for record in aln:
         checksum = seguid(record.seq)
         if checksum in checksums:
-            print ("Ignoring %s" % record.id)
+            #print ("Ignoring %s" % record.id)
             continue
         else:
             checksums.append(checksum)
@@ -63,13 +77,30 @@ def remove_dup_seqs(aln):
 
     return(Bio.Align.MultipleSeqAlignment(aln_no_dup))
 
+
+def export_nexus(ls_alns, outfilename):
+
+    nexi = []
+    for aln in ls_alns:
+        output = StringIO()
+        AlignIO.write(aln[1], output, 'nexus')
+        p = Nexus.Nexus()
+        p.read(output.getvalue())
     
+        nexi.append((aln[0], p))
+
+    combined = Nexus.combine(nexi)
+    combined.write_nexus_data(outfilename, interleave=True)
 
 
+
+########## WORKING
 
 data = '/Users/Tagliacollo/Google-Drive/05-G-forms/raw-data/aln/mtx/GymnSuperMTXAll.nex'
 #data = '/Users/Tagliacollo/Google-Drive/05-G-forms/raw-data/aln/mtx/TestMTX.nex'
-aln = charset_aln(data)
-aln = aln[3]
-x1  = cut_gap_in_blocks(aln, 0.98)
-y   = remove_dup_seqs(x1)
+cut  = 0.60
+outfilename = '/Users/Tagliacollo/Google-Drive/05-G-forms/raw-data/aln/mtx/processed/GymnSuperMTXAll60.nex'
+
+processed_data(data, cut, outfilename)
+
+
